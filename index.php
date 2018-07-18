2a8b2417386bef9a9f5868be819cd4d6e4770e0d
Anna <annalarissa.martins@hotmail.com>
2018-07-18 14:42:27 -0300

    Arquivos

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Projeto 01</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Descrição do site">
+	<meta name="keywords" content="Laboratório, Saúde, DF">
+	<meta charset="utf-8">
+</head>
+<body>
+	<header>
+		<div class="center">
+			<div class="logo">LogoMarca</div>
+			<nav class="desktop">
+				<ul>
+					<li><a href="#">Sobre nós</a></li>
+					<li><a href="#">Exames</a></li>
+					<li><a href="#">Localização & Contato</a></li>
+					<li><a href="#">Resultados de Exames</a></li>
+				</ul>
+			</nav>
+			<nav class="mobile">
+				<ul>
+					<li><a href="#">Sobre nós</a></li>
+					<li><a href="#">Exames</a></li>
+					<li><a href="#">Localização & Contato</a></li>
+					<li><a href="#">Resultados de Exames</a></li>
+				</ul>
+			</nav>
+		</div>		
+	</header>
+
+	<section class="banner-principal">
+		<div class="center">
+			<form>
+				<h2>E-mail</h2>
+				<input type="email" name="email" required> 
+				<input type="submit" name="acao" value="Cadastrar!">
+			</form>
+		</div>
+	</section>
+
+	<section class="descricao-lab">
+		<div class="center">
+			<div class="w50">
+				<h2>Laboratório Saúde</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vulputate scelerisque nulla eu eleifend. Phasellus eget nisl consectetur elit pretium efficitur. Aenean enim sapien, venenatis eu orci vitae, maximus dignissim mi. Nullam vehicula nulla at nibh fermentum rutrum. In lacus sem, porttitor non urna maximus, volutpat feugiat erat. </p>
+			</div>
+			<div class="w50">
+				<img src="images/logoLab.png">
+			</div>
+		</div>
+	</section>
+
+	<section>
+		<div class="center">
+			<div class="box-especialidades">
+				<h3>Ícone 1</h3>
+				<h3>Esp. 1</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vulputate scelerisque nulla eu eleifend. Phasellus eget nisl consectetur elit pretium efficitur. Aenean enim sapien, venenatis eu orci vitae, maximus dignissim mi. Nullam vehicula nulla at nibh fermentum rutrum. In lacus sem, porttitor non urna maximus, volutpat feugiat erat.</p>
+			</div>
+			<div class="box-especialidades">
+				<h3>Ícone 2</h3>
+				<h3>Esp. 2</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vulputate scelerisque nulla eu eleifend. Phasellus eget nisl consectetur elit pretium efficitur. Aenean enim sapien, venenatis eu orci vitae, maximus dignissim mi. Nullam vehicula nulla at nibh fermentum rutrum. In lacus sem, porttitor non urna maximus, volutpat feugiat erat.</p>
+			</div>
+			<div class="box-especialidades">
+				<h3>Ícone 3</h3>
+				<h3>Esp. 3</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vulputate scelerisque nulla eu eleifend. Phasellus eget nisl consectetur elit pretium efficitur. Aenean enim sapien, venenatis eu orci vitae, maximus dignissim mi. Nullam vehicula nulla at nibh fermentum rutrum. In lacus sem, porttitor non urna maximus, volutpat feugiat erat.</p>
+			</div>
+		</div>
+	</section>
+</body>
+</html>
